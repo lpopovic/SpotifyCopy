ee90119ac600c832f7d6cb2342c3c9ab57cbc8e9
Lazar Popovic <lazar.popovic.riitech.com>
2021-03-24 19:05:24 +0100

    component UI

---
+++ Spotify/ViewModels/SearchResultSubtitleTableViewCellViewModal.swift
@@ -0,0 +1,14 @@
+//
+//  SearchresultSubtitleTableViewCellViewModel.swift
+//  Spotify
+//
+//  Created by MacBook on 3/24/21.
+//
+
+import Foundation
+
+struct SearchResultSubtitleTableViewCellViewModel {
+    let title: String
+    let subtitle: String
+    let imageURL: URL?
+}
