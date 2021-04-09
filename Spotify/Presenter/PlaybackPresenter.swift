6257ef5bf51bf6feb0650bf1189377b7af2e2e4e
Lazar Popovic <lazar.popovic.riitech.com>
2021-04-09 02:43:40 +0200

    component UI

--- Spotify/Presenter/PlaybackPresenter.swift
+++ Spotify/Presenter/PlaybackPresenter.swift
@@ -27,7 +27,7 @@ final class PlaybackPresenter {
     var currentTrack: AudioTrack? {
         if let track = track, tracks.isEmpty {
             return track
-        } else if let player = self.playerQueue, !tracks.isEmpty {
+        } else if let _ = self.playerQueue, !tracks.isEmpty {
             return tracks[index]
         }
         
